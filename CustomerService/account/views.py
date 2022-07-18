0efbe05728ee25246ee095bde7492ca1751cdc6b
mateuszgua <mateuszgua@wp.eu>
2022-07-18 21:03:54 +0200

    Initial commit

---
+++ CustomerService/account/views.py
@@ -0,0 +1,30 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'],
+                                password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Logowanie zakończyło się sukcesem.')
+                else:
+                    return HttpResponse('Konto jest zablokowane.')
+            else:
+                return HttpResponse('Błędne dane logowania.')
+    else:
+        form = LoginForm()
+    return render(request, 'account/login.html', {'form': form})
+
+@login_required
+def dashboard(request):
+    return render(request,
+                  'account/dashboard.html',
+                  {'section': 'dashboard'})
