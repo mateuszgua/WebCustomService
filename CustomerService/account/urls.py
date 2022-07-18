2a85e60a1ae7023046a8bbd3638053249a38c0ba
mateuszgua <mateuszgua@wp.eu>
2022-07-18 21:39:37 +0200

    Add password change

--- CustomerService/account/urls.py
+++ CustomerService/account/urls.py
@@ -3,8 +3,12 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
+    #Login and logout
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    #Dashboard
     path('', views.dashboard, name='dashboard'),
-
+    #Change password
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 ]
